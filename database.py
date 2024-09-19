a7940dd0ebc3b9beaa5ba29482caf5faeaaeda12
Andron1992 <nagola2112@gmail.com>
2024-09-19 12:45:27 +0200

    DZ12web

--- database.py
+++ database.py
@@ -1,13 +1,17 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
+import os
 
-SQLALCHEMY_DATABASE_URL = "postgresql+psycopg2://postgres:11111@localhost:5432/contacts"
+# Завантаження змінних з .env файлу
+load_dotenv()
+
+SQLALCHEMY_DATABASE_URL = os.getenv("SQLALCHEMY_DATABASE_URL")
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
-
 
 def get_db():
     db = SessionLocal()
